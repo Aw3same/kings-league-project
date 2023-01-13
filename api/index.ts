c43d02637090d05eafa411e23e87c950460a3bd8
Ángel Barroso Sanz <angel.barroso@linguaserve.com>
2023-01-13 17:44:03 +0100

    Add new endpoints

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,38 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
 import leaderboard from '../db/leaderboard.json'
-
+import presidents from '../db/presidents.json'
+import teams from '../db/teams.json'
 
 const app = new Hono()
 
-app.get('/', (context) => context.json([
-	{
-		endpoint: '/leaderboard',
-		description: 'Returns the leaderboard'
-	}
-]))
-app.get('/leaderboard', (c) => c.json(leaderboard))
+app.get('/', context =>
+  context.json([
+    {
+      endpoint: '/leaderboard',
+      description: 'Returns the leaderboard',
+    },
+    {
+      endpoint: '/teams',
+      description: 'Returns the teams',
+    },
+    {
+      endpoint: '/presidents',
+      description: 'Returns the presidents',
+    },
+  ])
+)
+app.get('/leaderboard', c => c.json(leaderboard))
+app.get('/presidents', c => c.json(presidents))
+app.get('/presidents/:id', c => {
+  const id = c.req.param('id')
+  const foundPresident = presidents.find(president => president.id === id)
+  return foundPresident
+    ? c.json(foundPresident)
+    : c.json({ message: 'president not found' }, 404)
+})
+app.get('/teams', c => c.json(teams))
 
-app.get('/static/*', serveStatic({ root: './'}))
+app.get('/static/*', serveStatic({ root: './' }))
 
 export default app
-
-
